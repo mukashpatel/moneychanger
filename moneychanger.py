c4660a156d5d35d7e3dd0ce939e8b1f1b7988396
jonfernandes <jonfernandes@gmail.com>
2025-01-11 20:45:29 +0000

    Updated call_llm

--- moneychanger.py
+++ moneychanger.py
@@ -5,22 +5,20 @@ from dotenv import load_dotenv
 import requests
 import json
 import streamlit as st
+import os
+from openai import OpenAI
+
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.inference.ai.azure.com"
+model_name = "gpt-4o-mini"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
 
 load_dotenv()
 EXCHANGERATE_API_KEY = os.getenv('EXCHANGERATE_API_KEY')
-
-# Title of the app
-st.title("Multilingual Money Changer")
-
-# Text box for user input
-user_input = st.text_input("Enter the amount and the currency")
-
-# Submit button
-if st.button("Submit"):
-    # Display the input text below the text box
-    st.write("You entered:", user_input)
-
-
 
 def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
     """Return a tuple of (base, target, amount, conversion_result (2 decimal places))"""
@@ -32,11 +30,28 @@ def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
     try:
-        completion = ...
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant.",
+                },
+                {
+                    "role": "user",
+                    "content": textbox_input,
+                }
+            ],
+            temperature=1.0,
+            top_p=1.0,
+            max_tokens=1000,
+            model=model_name
+        )
+
+    
     except Exception as e:
         print(f"Exception {e} for {text}")
     else:
-        return completion
+        return response.choices[0].message.content
 
 def run_pipeline():
     """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
@@ -51,3 +66,14 @@ def run_pipeline():
         st.write(f"(Function calling not used) and response from the model")
     else:
         st.write("NotImplemented")
+
+# Title of the app
+st.title("Multilingual Money Changer")
+
+# Text box for user input
+user_input = st.text_input("Enter the amount and the currency")
+
+# Submit button
+if st.button("Submit"):
+    # Display the input text below the text box
+    st.write(call_llm(user_input))
